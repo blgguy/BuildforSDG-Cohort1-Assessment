9be511814a37042a7628aae1b1d72dec5b8f476a
Aminu Muhmmed Bulangu <bulangu97@gmail.com>
2020-04-14 19:36:58 +0100

    still making some changes in the estimator - challenge 3

--- src/estimator.php
+++ src/estimator.php
@@ -18,7 +18,9 @@ function covid19ImpactEstimator($data)
     $beds = (35*$totalHospitalBeds) / 100;
     $impacthospitalBedsByRequestedTime = bcdiv($beds - $impactsevereCaseByRequestedTime, 1, 0);
     //$impacthospitalBedsByRequestedTime = round($impacthospitalBedsByRequestedTime);
-    $impactcasesForVentilatorsByRequestedTime = floor((5 * $impactInfectionsByRequestedTime) / 100);
+
+    $impactcasesForICUByRequestedTime	= floor((5 * $impactInfectionsByRequestedTime) / 100);
+    $impactcasesForVentilatorsByRequestedTime = floor((2 * $impactInfectionsByRequestedTime) / 100);
     $impactdollarsInFlight = dollarsInFlight($impactInfectionsByRequestedTime, $periodtype, $timetoelapse, $avgDailyIncomeInUSD, $avgDailyIncomeInPopulation);
 
     //severeimpact...
@@ -28,6 +30,8 @@ function covid19ImpactEstimator($data)
     $severebeds = (35*$totalHospitalBeds) / 100;
 	$servereimpacthospitalBedsByRequestedTime = bcdiv($severebeds - $servereimpactsevereCaseByRequestedTime, 1, 0);
 	//$servereimpacthospitalBedsByRequestedTime = round($servereimpacthospitalBedsByRequestedTime);
+
+	$severeimpactcasesForICUByRequestedTime = floor((5 * $severeImpactInfectionsByRequestedTime) / 100);
 	$severeimpactcasesForVentilatorsByRequestedTime = floor((2 * $severeImpactInfectionsByRequestedTime) / 100);
     $severeimpactdollarsInFlight = dollarsInFlight($severeImpactInfectionsByRequestedTime, $periodtype, $timetoelapse, $avgDailyIncomeInUSD, $avgDailyIncomeInPopulation);
 
@@ -37,6 +41,7 @@ function covid19ImpactEstimator($data)
 		'infectionsByRequestedTime' 	=> (int)$impactInfectionsByRequestedTime,
 		'severeCasesByRequestedTime' 	=> (int)$impactsevereCaseByRequestedTime,
 		'hospitalBedsByRequestedTime' 	=> (int)$impacthospitalBedsByRequestedTime,
+		'casesForICUByRequestedTime'	=> (int)$impactcasesForICUByRequestedTime,
 		'casesForVentilatorsByRequestedTime' => (int)$impactcasesForVentilatorsByRequestedTime,
 		'dollarsInFlight' 				=> (int)$impactdollarsInFlight,
 	);
@@ -47,6 +52,7 @@ function covid19ImpactEstimator($data)
 		'infectionsByRequestedTime' 	=> (int)$severeImpactInfectionsByRequestedTime,
 		'severeCasesByRequestedTime' 	=> (int)$servereimpactsevereCaseByRequestedTime,
 		'hospitalBedsByRequestedTime' 	=> (int)$servereimpacthospitalBedsByRequestedTime,
+		'casesForICUByRequestedTime'	=> (int)$severeimpactcasesForICUByRequestedTime,
 		'casesForVentilatorsByRequestedTime' => (int)$severeimpactcasesForVentilatorsByRequestedTime,
 		'dollarsInFlight' 				=> (int)$severeimpactdollarsInFlight,
 	);
