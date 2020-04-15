15d47cab9e161cfdd4bc82cbe20c0312182c877e
Aminu Muhmmed Bulangu <bulangu97@gmail.com>
2020-04-15 02:49:16 +0100

    still making some changes to the api - challenge 5

--- src/estimator.php
+++ src/estimator.php
@@ -95,15 +95,3 @@ function dollarsInFlight($infectionsByRequestedTime,$periodtype, $timetoelapse,
     $Avaragedollars =  ($infectionsByRequestedTime * $avgDailyIncomeInUSD * $avgDailyIncomePopulation) / $Totaldays;
     return floor($Avaragedollars);
 }
-
- function log(){
-	$time2 = microtime(true);
-	$file = 'log.txt';
-	$logmsg = file_get_contents($file);
-	$exe_time = "0".(int)($time2 - $_SERVER["REQUEST_TIME_FLOAT"])* 1000;
- 	$logmsg = $_SERVER['REQUEST_METHOD']. "\t\t".$_SERVER['REQUEST_URI']. "\t\t".http_response_code()."\t\t". $exe_time."ms";
-
-	$outputMessage =file_put_contents($file, $logmsg."\n", FILE_APPEND | LOCK_EX);
-
-	return $outputMessage;
-	}
